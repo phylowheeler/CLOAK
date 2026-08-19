ba064117a04a643d1b1448fa908d530b9626369d
Andrew Wheeler <105318561+phylowheeler@users.noreply.github.com>
2026-08-19 16:59:01 -0400

    Add files via upload

--- Tree_inference+comparison/figure5_stats.R
+++ Tree_inference+comparison/figure5_stats.R
@@ -1,10 +1,10 @@
 figure5 <- read.csv("figure5.csv")
 
-figure5_filtered=filter(figure5,variable='filtered')
-figure5_unfiltered=filter(figure5,variable='unfiltered')
-unfiltered1=filter(figure5_unfiltered,genes==1)
-unfiltered4=filter(figure5_unfiltered,genes==4)
-unfiltered8=filter(figure5_unfiltered,genes==8)
+figure5_filtered=filter(figure5,variable=='cleaned')
+figure5_unfiltered=filter(figure5,variable=='uncleaned')
+unfiltered1=filter(figure5_unfiltered,genes=="1")
+unfiltered4=filter(figure5_unfiltered,genes=="4")
+unfiltered8=filter(figure5_unfiltered,genes=="8")
 
 cor.test(unfiltered1$distance, unfiltered1$length, method="spearman")
 cor.test(unfiltered4$distance, unfiltered4$length, method="pearson")
