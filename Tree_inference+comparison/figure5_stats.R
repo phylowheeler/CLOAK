554c0cf24d2d6e4a81195d39cb6c8a24b1506520
Andrew Wheeler <105318561+phylowheeler@users.noreply.github.com>
2026-08-19 15:36:55 -0400

    Add files via upload

---
+++ Tree_inference+comparison/figure5_stats.R
@@ -0,0 +1,15 @@
+figure5 <- read.csv("figure5.csv")
+
+figure5_filtered=filter(figure5,variable='filtered')
+figure5_unfiltered=filter(figure5,variable='unfiltered')
+unfiltered1=filter(figure5_unfiltered,genes==1)
+unfiltered4=filter(figure5_unfiltered,genes==4)
+unfiltered8=filter(figure5_unfiltered,genes==8)
+
+cor.test(unfiltered1$distance, unfiltered1$length, method="spearman")
+cor.test(unfiltered4$distance, unfiltered4$length, method="pearson")
+cor.test(unfiltered8$distance, unfiltered8$length, method="pearson")
+
+
+t.test(unfiltered1$distance, unfiltered4$distance)
+t.test(unfiltered4$distance, unfiltered8$distance)
