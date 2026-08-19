7cb6a14e538d943e22a48fe3d5ccc4627df89f20
Andrew Wheeler <105318561+phylowheeler@users.noreply.github.com>
2026-08-19 14:12:01 -0400

    Add files via upload

---
+++ Tree_inference+comparison/Tree_distance_data/tree_dist_stats.R
@@ -0,0 +1,43 @@
+
+
+
+lrm <- read.csv("lrm_distance.csv")
+quartet <- read.csv("quartet.csv")
+dlt <- read.csv("dlt.csv")
+
+
+# Lin-Rajan-Moret t tests
+t.test(lrm$uncleaned,lrm$cloak,paired=TRUE)
+t.test(lrm$uncleaned,lrm$divvier,paired=TRUE)
+t.test(lrm$uncleaned,lrm$taper,paired=TRUE)
+t.test(lrm$uncleaned,lrm$divvierpf,paired=TRUE)
+t.test(lrm$divvierpf,lrm$gene_cloak,paired=TRUE)
+t.test(lrm$divvierpf,lrm$gene_divvier,paired=TRUE)
+t.test(lrm$divvierpf,lrm$gene_taper,paired=TRUE)
+t.test(lrm$divvierpf,lrm$gene_divvierpf,paired=TRUE)
+t.test(lrm$divvierpf,lrm$propagate,paired=TRUE)
+t.test(lrm$gene_cloak,lrm$gtrpmix,paired=TRUE)
+
+# quartet support t tests
+t.test(quartet$uncleaned,quartet$cloak,paired=TRUE)
+t.test(quartet$uncleaned,quartet$divvier,paired=TRUE)
+t.test(quartet$uncleaned,quartet$taper,paired=TRUE)
+t.test(quartet$uncleaned,quartet$divvierpf,paired=TRUE)
+t.test(quartet$divvierpf,quartet$gene_cloak,paired=TRUE)
+t.test(quartet$divvierpf,quartet$gene_divvier,paired=TRUE)
+t.test(quartet$divvierpf,quartet$gene_taper,paired=TRUE)
+t.test(quartet$divvierpf,quartet$gene_divvierpf,paired=TRUE)
+t.test(quartet$divvierpf,quartet$propagate,paired=TRUE)
+t.test(quartet$gene_cloak,quartet$gtrpmix,paired=TRUE)
+
+# Duplication/Loss/Transfer t-tests
+t.test(dlt$uncleaned,dlt$cloak,paired=TRUE)
+t.test(dlt$uncleaned,dlt$divvier,paired=TRUE)
+t.test(dlt$uncleaned,dlt$taper,paired=TRUE)
+t.test(dlt$uncleaned,dlt$divvierpf,paired=TRUE)
+t.test(dlt$divvierpf,dlt$gene_cloak,paired=TRUE)
+t.test(dlt$divvierpf,dlt$gene_divvier,paired=TRUE)
+t.test(dlt$divvierpf,dlt$gene_taper,paired=TRUE)
+t.test(dlt$divvierpf,dlt$gene_divvierpf,paired=TRUE)
+t.test(dlt$divvierpf,dlt$propagate,paired=TRUE)
+t.test(dlt$gene_cloak,dlt$gtrpmix,paired=TRUE)
